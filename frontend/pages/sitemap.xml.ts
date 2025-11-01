976d97a97bf8eab5cefb7f05da9c3c8c4da6407e
MeerAzmaanAli <azmaan.ali11@gmail.com>
2025-11-01 22:49:56 +0530

    first commit

--- frontend/pages/sitemap.xml.ts
+++ frontend/pages/sitemap.xml.ts
@@ -1,4 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { keywordPages } from '../utils/seo'
 
 const SITE = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
@@ -15,9 +14,14 @@ function generateSiteMap() {
   return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${body}</urlset>`
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export async function getServerSideProps({ res }: { res: any }) {
   const xml = generateSiteMap()
   res.setHeader('Content-Type', 'application/xml')
   res.write(xml)
   res.end()
+  return { props: {} }
+}
+
+export default function Sitemap() {
+  return null
 }
